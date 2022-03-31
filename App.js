90cdb759fe4a95a1ed37486db2f2d5a7819b3680
Germano Pereira <germano.pereira@bit.com.br>
2022-03-31 14:01:23 -0300

    bora

--- App.js
+++ App.js
@@ -1,189 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
-import React, { useState } from 'react';
-
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-
-
-
-export default function App() {
-
-  const [nome, setNome] = useState('');
-
-  const [showEmoji, setShowEmoji] = useState(false);
-
-
-
-  function handleDescriptionChange(pnome) {
-
-    setNome(pnome);
-
-    if (pnome.length > 0)
-
-      setShowEmoji(true);
-
-    else
-
-      setShowEmoji(false);
-
+export default class App extends React.Component {
+  
+  constructor(props){
+    super(props)
+    this.state = {nome:"", peso: '', altura: '', info: '-', resultado: 0.0}
+    this.calculaIMC = this.calculaIMC.bind(this)
+  }
+  
+  calculaIMC(){
+    let imc = this.state.peso / (this.state.altura * this.state.altura)
+    let s = this.state
+    s.resultado = imc
+    if(s.resultado < 18.5){
+      s.info ='Menor que 18,5, seu quadro é de: Magreza'
+    }
+    else if (s.resultado < 24.9){
+     s.info ='Entre 18,5 e 24,9, seu quadro é: Normal'
+    }
+    else if (s.resultado < 29.9){
+     s.info ='Entre 25,0 e 29,9, você está com: Sobrepeso'
+    }
+    else if (s.resultado < 39.9) {
+     s.info ='Entre 30,0 e 39,9, tome cuidado, você está com: Obesidade'
+    }
+    else if (s.resultado > 39.9) {
+     s.info ='Maior que 40,0, você está com: Obesidade Grave'
+    }
+    this.setState(s)
   }
 
-
-
-  function handleButtonPress() {
-
-    alert('oi ' + nome);
-
+  clear = () => {
+    this.setState({
+      nome:"",
+      peso: '',
+      altura: '',
+      resultado: 0.0,
+      info: '-'
+    })
   }
 
-
-
-  return (
-
-    <View style={styles.container}>
-
-      <Text style={styles.title}>Olá mundo!</Text>
-
-      <View style={styles.inputContainer}>
-
+  render() {
+    return (
+      <View style={styles.viewContainer}>
+         <Text style={styles.text}>Nome</Text>
         <TextInput
-
-          style={styles.input}
-
-          placeholder="Digite seu nome"
-
-          onChangeText={handleDescriptionChange}
-
+          style={styles.textInput}
+          onChangeText={nome => this.setState({ nome })}
+          value={this.state.nome}
+          placeholder='Exemplo: Germano'
+          keyboardType={'string'}
         />
-
-        <TouchableOpacity
-
-          style={styles.button}
-
-          onPress={handleButtonPress}
-
-        >
-
-          <Text style={styles.buttonText}>Enviar</Text>
-
-        </TouchableOpacity>
-
-        <Text style={styles.input}>{showEmoji}</Text>
-
-        <Text style={styles.input}>{nome}</Text>
-
-        <Text style={styles.input}>{nome.length}</Text>
-
-        {showEmoji ? <Text style={styles.input}>??</Text> : ''}
-
+        <separator/>
+        <Text style={styles.text}>Altura (m)</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={altura => this.setState({ altura })}
+          value={this.state.altura}
+          placeholder='Exemplo: 1,75'
+          keyboardType={'numeric'}
+        />
+        <Text style={styles.text}>Peso (kg)</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={peso => this.setState({ peso })}
+          value={this.state.peso}
+          placeholder='Exemplo: 68,8'
+          keyboardType={'numeric'}
+        />
+        <Separator />
+        <Button
+          onPress={this.calculaIMC}
+          title='Calcula'
+          color='green'
+          accessibilityLabel='Clique aqui para calcular seu IMC'
+        />
+        <Separator/>
+        <Button
+          onPress={this.clear}
+          title='Limpa'
+          color='red'
+          accessibilityLabel='Botão para limpar os valores'
+        />
+        <Separator />
+        <Text style={styles.input}>
+        {this,this.state.nome}, Seu IMC é: {this.state.resultado.toFixed(2)} {this.state.info}
+        </Text>
       </View>
-
-      <StatusBar style="auto" />
-
-    </View>
-
-  );
-
+    );
+  }
 }
 
-
+const Separator = () => (
+  <View style={styles.separator} />
+);
 
 const styles = StyleSheet.create({
-
   container: {
-
     flex: 1,
-
     backgroundColor: '#fff',
-
     alignItems: 'center',
-
     justifyContent: 'center',
-
   },
-
-  title: {
-
-    color: 'red',
-
-    fontSize: 20,
-
-    fontWeight: 'bold',
-
-    marginTop: 50,
-
-  },
-
-  inputContainer: {
-
-    flex: 1,
-
-    marginTop: 30,
-
-    width: '90%',
-
-    padding: 20,
-
-    borderTopLeftRadius: 10,
-
-    borderTopRightRadius: 10,
-
-    alignItems: 'stretch',
-
-    backgroundColor: '#fff'
-
-  },
-
   input: {
-
-    marginTop: 10,
-
-    height: 60,
-
-    backgroundColor: '#fff',
-
-    borderRadius: 10,
-
-    paddingHorizontal: 24,
-
-    fontSize: 16,
-
-    alignItems: 'stretch'
-
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 5,
+    fontSize: 25,
   },
-
-  button: {
-
-    marginTop: 10,
-
-    height: 60,
-
-    backgroundColor: 'red',
-
-    borderRadius: 10,
-
-    paddingHorizontal: 24,
-
-    fontSize: 16,
-
-    alignItems: 'center',
-
-    justifyContent: 'center',
-
-    elevation: 20,
-
-    shadowOpacity: 20,
-
-    shadowColor: '#ccc',
-
+  separator: {
+    marginVertical: 10,
+    borderBottomColor: 'black',
+    borderWidth: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
-
-  buttonText: {
-
-    color: '#fff',
-
-    fontWeight: 'bold',
-
+  textInput: {
+    height: 50,
+    borderWidth: 2,
+    borderColor: 'black',
+    paddingLeft: 10,
+    margin: 15,
+    borderRadius: 20
   }
-
 });
